0c255970789dfd8ac883b02f606bb2805f7aaeed
Chethana Kalpa <chethanakh1998.09@gmail.com>
2025-04-29 21:27:42 +0530

    feat(app): setup spark anlyis with ui

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM python:3.13-alpine
+FROM python:3.11-alpine
+
+RUN apk add --no-cache openjdk11 bash curl build-base
+
+ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+ENV PATH="$JAVA_HOME/bin:$PATH"
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import requests
 import pandas as pd
-
+from pyspark.sql import SparkSession
+import plotly.express as px
 
 def fetch_crypto_data():
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {
-        'vs_currency': 'usd',
-        'order': 'market_cap_desc',
-        'per_page': 50,
-        'page': 1,
-        'sparkline': False
+        'vs_currency': 'usd'
     }
     response = requests.get(url, params=params)
     data = response.json()
@@ -18,4 +15,26 @@ def fetch_crypto_data():
 
 st.title("Crypto Market Analytics Dashboard")
 
-fetch_crypto_data()
+spark = SparkSession.builder.appName("CryptoAnalytics").getOrCreate()
+
+df_pd = fetch_crypto_data()
+df_spark = spark.createDataFrame(df_pd)
+df_spark.createOrReplaceTempView("cryptos_temp")
+
+limit = st.select_slider("Select the number of top coins to display:", options=range(1, 21), value=10)
+
+top_coin = spark.sql("""
+    SELECT id, symbol, name, price_change_percentage_24h 
+    FROM cryptos_temp 
+    ORDER BY price_change_percentage_24h DESC 
+    LIMIT 
+"""+str(limit))
+
+top_coin.show()
+
+
+st.write("Top "+str(limit)+" Coin(s) in Last (24h)")
+st.dataframe(top_coin.toPandas())
+
+fig = px.bar(top_coin.toPandas(), x='name', y='price_change_percentage_24h', color='name')
+st.plotly_chart(fig)
