35d8248a75cce2117d2f63a7191efac4c8191f87
Chethana Kalpa <chethanakh1998.09@gmail.com>
2025-04-29 20:42:26 +0530

    feat(app): fetch data from free api

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import requests
+
+def fetch_crypto_data():
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        'vs_currency': 'usd',
+        'order': 'market_cap_desc',
+        'per_page': 50,
+        'page': 1,
+        'sparkline': False
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(data)
+    return data
 
 st.title("Crypto Market Analytics Dashboard")
+
+fetch_crypto_data()
